f55c4ab5db795fc3439c3c843202355f6cbdc8d5
tanushbhootra576 <tanushbhootratanush@gmail.com>
2026-06-16 11:31:13 +0530

    resume

--- src/components/AboutMe/MagicBento.jsx
+++ src/components/AboutMe/MagicBento.jsx
@@ -158,7 +158,7 @@ const MagicBento = () => {
                     <div className="resume-body">
                         <p>Specialized in building robust web apps & design systems.</p>
                         <div className="button-group">
-                            <a href="https://drive.google.com/file/d/1Msrl20xyCZGcOGclcvqkR9M_yVVZvdCq/view?usp=sharing" className="bento-btn primary">
+                            <a href="https://drive.google.com/file/d/10awXph6ePWViWAvDAxVteWAZ7t00yTy0/view?usp=sharing" className="bento-btn primary">
                                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M7 10l5 5 5-5M12 15V3" /></svg>
                                 Resume
                             </a>
